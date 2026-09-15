89eb3472963f13ca0012ec40368f5a3acd21e702
urielfodor21 <urielfodor21@gmail.com>
2026-09-15 01:28:05 -0300

    Update create-user.ts

--- api/create-user.ts
+++ api/create-user.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { getAdminClient, requireAdmin, nicknameToEmail } from './_lib/supabaseAdmin';
+import { getAdminClient, requireAdmin, nicknameToEmail } from './_lib/supabaseAdmin.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
